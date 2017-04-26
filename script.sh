ed5909bd4b70869a2f42a4cb1f9c469624b9c4ae
Aidan Hobson Sayers <aidanhs@cantab.net>
2017-04-26 20:29:36 +0100

    try again...

--- script.sh
+++ script.sh
@@ -8,4 +8,6 @@ tar xf openssl-1.0.2k.tar.gz
 cd openssl-1.0.2k
 sed -i .bak 's/Never mind.*$/\0\n\texit 1/' crypto/cmac/Makefile
 ./Configure no-dso no-ssl2 no-ssl3 darwin64-x86_64-cc
+cat crypto/cmac/Makefile
+sed -i .bak 's/Never mind.*$/\0\n\texit 1/' crypto/cmac/Makefile
 make
