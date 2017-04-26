8ac8fd17d5ecdd31740ec88540afcfd8e353d81c
Aidan Hobson Sayers <aidanhs@cantab.net>
2017-04-26 20:13:03 +0100

    script fix

--- script.sh
+++ script.sh
@@ -2,9 +2,10 @@
 set -o errexit
 set -o pipefail
 set -o nounset
-curl -sSL -O https://s3.amazonaws.com/rust-lang-ci/rust-ci-mirror/openssl-1.0.2k.tar.gz &&
-tar xf openssl-1.0.2k.tar.gz &&
-cd openssl-1.0.2k &&
-sed -i 's/Never mind.*$/\0\n\texit 1/g' crypto/cmac/Makefile &&
-./Configure no-dso no-ssl2 no-ssl3 darwin64-x86_64-cc &&
+set -o xtrace
+curl -sSL -O https://s3.amazonaws.com/rust-lang-ci/rust-ci-mirror/openssl-1.0.2k.tar.gz
+tar xf openssl-1.0.2k.tar.gz
+cd openssl-1.0.2k
+sed -i .bak 's/Never mind.*$/\0\n\texit 1/' crypto/cmac/Makefile
+./Configure no-dso no-ssl2 no-ssl3 darwin64-x86_64-cc
 make
